58da83c6e97de800aede4e6bbd856e3ba96f6549
Rinku1806 <155158181+Rinku1806@users.noreply.github.com>
2025-11-03 21:39:40 +0530

    Initial major commit

---
+++ src/test/resources/runner/allure_reports.bat
@@ -0,0 +1,10 @@
+cd C:\Users\bkumar\eclipse-workspace\PageObjectModel_Javab
+allure generate --clean -o "C:\Users\bkumar\eclipse-workspace\PageObjectModel_Javab\allure-report"
+timeout /t 10
+cd C:\Users\bkumar\eclipse-workspace\PageObjectModel_Javab\allure-results
+allure serve
+
+
+*************************************************************************************
+C:\Users\MSI > cd C:\Users\bkumar\eclipse-workspace\PageObjectModel_Javab
+C:\Users\bkumar\eclipse-workspace\PageObjectModel_Javab> allure serve ENTER
